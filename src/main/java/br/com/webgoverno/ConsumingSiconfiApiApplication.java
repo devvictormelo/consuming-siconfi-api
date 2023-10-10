9c369b562bf8aae944840b83d754619988c9aacb
Victor Hugo <wkvictormelo@gmail.com>
2023-10-10 09:37:37 -0300

    Initial commit

---
+++ src/main/java/br/com/webgoverno/ConsumingSiconfiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.webgoverno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingSiconfiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingSiconfiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/webgoverno/ConsumingSiconfiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.webgoverno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingSiconfiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
