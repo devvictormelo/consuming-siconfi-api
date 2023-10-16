544fc14b6a7cf58732dc3f0531984b75ae4d914c
Victor Hugo <wkvictormelo@gmail.com>
2023-10-16 08:52:39 -0300

    Initial commit

---
+++ src/main/java/br/com/webgoverno/model/Company.java
@@ -0,0 +1,15 @@
+package br.com.webgoverno.model;
+
+import java.io.Serializable;
+
+public class Company implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+ private String nome;
+
+ private Long codIbge;
+
+
+
+}
